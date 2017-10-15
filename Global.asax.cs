fcf8f373829e0a88f14e904f3c830da4153bff97
GatorYang <util@botframework.com>
2017-10-15 02:24:27 +0000

    prepare to setup source control

---
+++ Global.asax.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using Autofac;
+using System.Configuration;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Azure;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
+
+namespace SimpleEchoBot
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            var store = new TableBotDataStore(ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString);
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            
+            Conversation.UpdateContainer(
+           builder =>
+           {
+               builder.Register(c => store)
+                         .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
+                         .AsSelf()
+                         .SingleInstance();
+
+               builder.Register(c => new CachingBotDataStore(store,
+                          CachingBotDataStoreConsistencyPolicy
+                          .ETagBasedConsistency))
+                          .As<IBotDataStore<BotData>>()
+                          .AsSelf()
+                          .InstancePerLifetimeScope();
+
+
+           });  
+        }
+    }
+}
